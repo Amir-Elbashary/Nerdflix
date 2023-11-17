3785f187a0d803a53fe4b94c32f2aaa93fb43155
Amir El-Bashary <amir.elbashary@gmail.com>
2023-11-17 21:17:40 +0200

    using non module way

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { MoviesListComponent } from "./components/movies/movies-list/movies-list.component";
 
 const routes: Routes = [
-  // Basically we have nothing on homepage
-  // so it will jump to movies page, and i
-  // prefered to demonestrate the usage of modules
-  // as it suits the bigger application,
-  // It could be as simple as putting the route with
-  // the component here and just import it in app module
-  // and it's done
   {
     path: '',
     redirectTo: 'movies',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'movies',
-    loadChildren: () => import('./components/movies/movies.module').then(m => m.MoviesModule)
-  },
+    component: MoviesListComponent
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      routes,
-      { preloadingStrategy: PreloadAllModules }
-    )
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from "./layout/layout.module";
+import { HeaderComponent } from "./components/header/header.component";
+import { MoviesListComponent } from "./components/movies/movies-list/movies-list.component";
+import { MovieItemComponent } from "./components/movies/movie-item/movie-item.component";
+import { SharedModule } from "./components/shared/shared.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    LayoutModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
