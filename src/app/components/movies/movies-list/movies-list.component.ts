5aeea72174ae6ea088db909b60a2015529723251
Amir El-Bashary <amir.elbashary@gmail.com>
2023-11-17 15:55:16 +0200

    implement basic design for header

--- src/app/components/movies/movies-list/movies-list.component.ts
+++ src/app/components/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-movies-list',
@@ -6,9 +7,13 @@ import { Component, OnInit, Inject } from '@angular/core';
   styleUrls: ['./movies-list.component.css']
 })
 export class MoviesListComponent implements OnInit {
+  private URL = '../assets/imdb-top-50.json';
+
   constructor(
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
+    this.http.get(this.URL).subscribe(console.log);
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sansLight: "Sans-Light",
+        sansBold: "Sans-Bold"
+      }
+    },
   },
   plugins: [],
 }
